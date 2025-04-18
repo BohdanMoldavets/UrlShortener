8474652e5fa0c8baa3210f7002d68d4e41c941dd
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-04-18 18:16:52 +0200

    Add validation for input link

--- src/main/java/com/moldavets/url_shortener_api/controller/UrlController.java
+++ src/main/java/com/moldavets/url_shortener_api/controller/UrlController.java
@@ -1,16 +1,15 @@
 package com.moldavets.url_shortener_api.controller;
 
 import com.moldavets.url_shortener_api.model.dto.UrlRequestDto;
-import com.moldavets.url_shortener_api.model.dto.UrlResponseLongUrlDto;
 import com.moldavets.url_shortener_api.model.dto.UrlResponseShortUrlDto;
 import com.moldavets.url_shortener_api.service.Impl.UrlApplicationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/urls")
@@ -20,14 +19,14 @@ public class UrlController {
     private final UrlApplicationService urlApplicationService;
 
     @GetMapping("/{shortUrl}")
-    public ResponseEntity<Void> getLongUrl(@PathVariable("shortUrl") String shortUrl) {
+    public ResponseEntity<Void> redirectLongUrl(@PathVariable("shortUrl") String shortUrl) {
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(urlApplicationService.getLongUrl(shortUrl));
         return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 
     @PostMapping
-    public ResponseEntity<UrlResponseShortUrlDto> createShortUrl(@RequestBody UrlRequestDto urlRequestDto) {
+    public ResponseEntity<UrlResponseShortUrlDto> createShortUrl(@Valid @RequestBody UrlRequestDto urlRequestDto) {
         return new ResponseEntity<>(urlApplicationService.createShortUrl(urlRequestDto),HttpStatus.CREATED);
     }
 }
