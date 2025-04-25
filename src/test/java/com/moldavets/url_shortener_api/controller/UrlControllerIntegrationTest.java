30a744cfbedf151a06d6c5bfa19bca13e1ba6fd3
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-04-25 16:52:13 +0200

    Add new integration tests for UrlController

--- src/test/java/com/moldavets/url_shortener_api/controller/UrlControllerIntegrationTest.java
+++ src/test/java/com/moldavets/url_shortener_api/controller/UrlControllerIntegrationTest.java
@@ -1,11 +1,15 @@
 package com.moldavets.url_shortener_api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moldavets.url_shortener_api.model.dto.url.UrlRequestDto;
 import com.redis.testcontainers.RedisContainer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -33,7 +37,7 @@ class UrlControllerIntegrationTest {
     MockMvc mockMvc;
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    ObjectMapper objectMapper;
 
     @DynamicPropertySource
     static void setUpProperties(DynamicPropertyRegistry registry) {
@@ -78,5 +82,59 @@ class UrlControllerIntegrationTest {
                 .andExpect(MockMvcResultMatchers.status().isGone())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("The short link has expired"));
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "http://www.example.com, PnGyot, ACTIVE, 3",
+            "http://www.example.pl, fgwOrk, ACTIVE, 2",
+            "http://www.example.de, koZjFA, ACTIVE, 2",
+            "http://www.example.ua, oRZGxo, ACTIVE, 1",
+            "http://www.example.io, URMLhx, EXPIRED, 0",
+    })
+    void retrieveInfoOfShortUrl_shouldReturnOkAndInfoAboutUrlInResponse_whenInputContainsShortUrlStoredInDb(String longUrl,
+                                                                                                            String shortUrl,
+                                                                                                            String linkStatus,
+                                                                                                            Long totalClicks) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/urls/info/"+ shortUrl))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.long_url").value(longUrl))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.short_url").value(shortUrl))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.link_status").value(linkStatus))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total_clicks").value(totalClicks));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "PnGyo2",
+            "fgwOrX",
+            "koZjFk",
+    })
+    void retrieveInfoOfShortUrl_shouldReturnNotFoundErrorInfoInResponse_whenInputContainsNotExistShortUrl(String shortUrl) throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/urls/info/"+ shortUrl))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Link not found"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "http://www.source.com",
+            "http://www.source.pl",
+            "https://www.source.de",
+            "https://www.source.ua",
+            "https://www.source.io"
+    })
+    void createShortUrl_shouldReturnShortUrl_whenInputContainsValidLongUrl(String longUrl) throws Exception {
+        UrlRequestDto requestDto = new UrlRequestDto(longUrl);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/urls")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(resultMvc ->
+                        Assertions.assertEquals(34, resultMvc.getResponse().getContentAsString().length()));
+    }
+
+
 
 }
