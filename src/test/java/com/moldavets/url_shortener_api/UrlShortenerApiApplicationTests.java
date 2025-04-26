0c57407c510c750a45e85133b6484585f606e983
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2025-04-26 16:00:58 +0200

    Add Dockerfile

--- src/test/java/com/moldavets/url_shortener_api/UrlShortenerApiApplicationTests.java
+++ src/test/java/com/moldavets/url_shortener_api/UrlShortenerApiApplicationTests.java
@@ -1,10 +1,8 @@
 package com.moldavets.url_shortener_api;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
 @SpringBootTest
 class UrlShortenerApiApplicationTests {
 
