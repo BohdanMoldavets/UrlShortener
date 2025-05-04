7f88d73ff0116748c1b9a28ffcfa5dd217279d07
Stanislav <stasikrybcev@gmail.com>
2025-05-04 03:03:03 +0200

    Creating a header on index.html with styles

--- frontend/react/src/reacts/Home.jsx
+++ frontend/react/src/reacts/Home.jsx
@@ -3,7 +3,6 @@ import LinkShortener from './LinkShortener';
 export const Home = () => {
     return (
         <div>
-            <h1>Главная</h1>
             <LinkShortener />
         </div>
     );
