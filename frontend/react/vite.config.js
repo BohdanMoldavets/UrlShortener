959401025706257a7518dda55dd062e248e7d70f
Stanislav <stasikrybcev@gmail.com>
2025-05-05 18:00:41 +0200

    Improved code to reduce

--- frontend/react/vite.config.js
+++ frontend/react/vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080/',
-    },
-    historyApiFallback: true
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      }
+    }
   }
-})
+});
+
