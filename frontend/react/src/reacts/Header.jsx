8b22048f5f1db70ea226586807c4291c747c2350
Stanislav <stasikrybcev@gmail.com>
2025-05-09 19:24:35 +0200

    Change theme by clicking on icons in footer

--- frontend/react/src/reacts/Header.jsx
+++ frontend/react/src/reacts/Header.jsx
@@ -1,20 +1,32 @@
 import { Link } from 'react-router-dom';
+import '../sass/blocks/header.scss';
+import { useTheme } from './ThemeContext';
 
-export const Header = () => {
+export const Header = ({ isAboutPage, aboutContactText }) => {
+    const { theme } = useTheme();
+
     return (
         <>
             <header className="header">
                 <div className="header__logo">
-                    <img src="/src/logo/logoDark.png" alt="logo" />
+                    <Link to="/">
+                        <img src={`/src/logo/logo${theme === 'light' ? 'Light' : 'Dark'}.png`} alt="logo" />
+                    </Link>
                 </div>
                 <div className="header__wrapper">
                     <div className="header__page">
                         <Link to="/" className="header__page-list">Shorten</Link>
-                        <Link to="/about" className="header__page-list">About</Link>
+                        <Link to="/about" className={`header__page-list ${isAboutPage ? 'header__page-list--active' : ''}`}>About</Link>
                     </div>
                     <div className="header__contact">
-                        <span className="header__mail">supportsomelnk@gmail.com</span>
-                        <span className='icon-clone header__icon'></span>
+                        {isAboutPage ? (
+                            <span className="header__mail">{aboutContactText}</span>
+                        ) : (
+                            <>
+                                <span className="header__mail">supportsomelnk@gmail.com</span>
+                                <span className='icon-clone header__icon'></span>
+                            </>
+                        )}
                     </div>
                 </div>
             </header>
@@ -23,4 +35,3 @@ export const Header = () => {
         </>
     );
 };
-
