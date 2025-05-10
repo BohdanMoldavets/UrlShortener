57245f1b97ebc90b46915733e6660357d7ce79f8
erfub1120 <ivan99001144@gmail.com>
2025-05-11 00:23:19 +0200

    changed target url to container and added host:true

--- frontend/react/vite.config.js
+++ frontend/react/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://urlShortener-backend-container:8080',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
